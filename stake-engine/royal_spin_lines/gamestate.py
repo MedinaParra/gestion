26dfe23129b88888a07ca08d88bc67879d7094e7
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-07-23 00:13:54 -0400

    Definir ronda independiente del slot

---
+++ stake-engine/royal_spin_lines/gamestate.py
@@ -0,0 +1,17 @@
+from game_override import GameStateOverride
+
+
+class GameState(GameStateOverride):
+    """One independent Stake Engine round: reveal, evaluate lines, close."""
+
+    def run_spin(self, sim, simulation_seed=None):
+        self.reset_seed(sim, simulation_seed)
+        self.repeat = True
+        while self.repeat:
+            self.reset_book()
+            self.draw_board()
+            self.evaluate_lines_board()
+            self.win_manager.update_gametype_wins(self.gametype)
+            self.evaluate_finalwin()
+            self.check_repeat()
+        self.imprint_wins()
